a1c6f738a05a1ddb9a4be00db1b47b403a84c16b
pioggia73 <monikawasilewska13@wp.pl>
2024-03-06 21:32:07 +0100

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
